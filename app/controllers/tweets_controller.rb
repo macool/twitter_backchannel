3f2c64ad4bf6a327060ad210d3782a7f6ee41c31
Mario Andrés Correa <admin@macool.me>
2013-07-02 13:02:39 -0500

    trying to fix bug when no records

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,7 +1,11 @@
 class TweetsController < ApplicationController
   
   def index
-    since_id = Tweet.order("id_str DESC").first(select: :id_str).id_str.to_i
+    since_id = if Tweet.count > 0
+      Tweet.order("id_str DESC").first(select: :id_str).id_str.to_i
+    else
+      1
+    end
     tweets = Twitter.search(APP_CONFIG["twitter"]["query"], :since_id => since_id)
     tweets.results.each do |tweet|
       Tweet.create tweet.attrs
