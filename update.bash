061e5e268e97864fb33b9516e4d550910d210224
Mario Andrés Correa <admin@macool.me>
2014-02-21 03:40:01 -0500

    update script

--- update.bash
+++ update.bash
@@ -1,9 +1,7 @@
 #!/bin/bash
 # git version:
 git pull
-# svn version:
-svn update
 bundle install
 rake db:migrate RAILS_ENV=production
-rake assets:precompile
-service apache2 reload
+rake assets:precompile RAILS_ENV=production
+touch tmp/restart.txt
